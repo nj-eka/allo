0515cde70439a43f3c83c5bdc41cd0ae4fb2908e
JN <70240447+nj-eka@users.noreply.github.com>
2023-08-09 08:09:31 +0300

    Update slist_gtest.cpp

--- src/cont/slist_gtest.cpp
+++ src/cont/slist_gtest.cpp
@@ -19,14 +19,6 @@ TEST(cont_unit_tests, slist_push) {
   EXPECT_EQ(ss.str(), "40-30-10-20-");
 }
 
-TEST(cont_unit_tests, slist_ctor_T) {
-  slist<int> sl(10);
-  std::iota(sl.begin(), sl.end(), 0);
-  std::stringstream ss;
-  std::copy(sl.begin(), sl.end(), std::ostream_iterator<int>(ss, "-"));
-  EXPECT_EQ(ss.str(), "0-1-2-3-4-5-6-7-8-9-");
-}
-
 TEST(cont_unit_tests, slist_bpool_push) {
   // std::set_terminate(&my_terminate_handler);
   slist<int, alloc::bpool_alloc<int, 10>> sl;
